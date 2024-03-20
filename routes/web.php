05a955c6f23abfaee1ca9492baff399d5dfaafae
carmelinfanta <carmelinfanta97@gmail.com>
2024-03-20 14:09:45 +0530

    updated middleware

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@ Route::get('/', function () {
 
 
 Route::get('/login', [UserAuthController::class, 'login'])->middleware('alreadyLoggedIn');
-Route::get('/register', [UserAuthController::class, 'register']);
-Route::post('/register-user', [UserAuthController::class, 'registerUser']);
+Route::get('/register', [UserAuthController::class, 'register'])->middleware('alreadyLoggedIn');
+Route::post('/register-user', [UserAuthController::class, 'registerUser'])->middleware('alreadyLoggedIn');
 Route::post('/login-user', [UserAuthController::class, 'loginUser']);
 Route::get('/dashboard', [UserAuthController::class, 'dashboard'])->middleware('isLoggedIn');
-Route::get('/signout', [UserAuthController::class, 'signout']);
+Route::get('/signout', [UserAuthController::class, 'signout'])->middleware('isLoggedIn');
 Route::post('/checkout/{id}', [PlanController::class, 'checkout']);
 Route::get('/purchasedPlans', [PlanController::class, 'purchasedPlans'])->middleware('isLoggedIn');
-Route::get('/paymentSuccess', [PlanController::class, 'paymentSuccess']);
-Route::get('/transaction', [PlanController::class, 'transaction']);
-Route::get('/profile', [PlanController::class, 'profile']);
+Route::get('/paymentSuccess', [PlanController::class, 'paymentSuccess'])->middleware('isLoggedIn');
+Route::get('/transaction', [PlanController::class, 'transaction'])->middleware('isLoggedIn');
+Route::get('/profile', [PlanController::class, 'profile'])->middleware('isLoggedIn');
 Route::post('/webhook', [StripeController::class, 'webhook']);
 Route::post('/checkout', [StripeController::class, 'checkoutSession']);
